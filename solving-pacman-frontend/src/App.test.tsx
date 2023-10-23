3a4c7d2cd7952684323976e0e1cbd207888bec5c
David <davidkidd.dev@gmail.com>
2023-10-23 16:04:26 +0100

    refacted project and test suite from js to tsx

--- solving-pacman-frontend/src/App.test.tsx
+++ solving-pacman-frontend/src/App.test.tsx
@@ -1,3 +1,10 @@
-test("Dummy Test", () => {
-    expect(1).toEqual(1);
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import App from "./App";
+
+test("renders learn react link", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
