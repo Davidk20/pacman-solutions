63427189c8120e9e844db309f46046788f786c30
David Kidd <zhac086@live.rhul.ac.uk>
2023-11-06 23:43:48 +0000

    created test to check a map can be returned

--- solving-pacman-backend/tests/services/level_handler_test.py
+++ solving-pacman-backend/tests/services/level_handler_test.py
@@ -12,3 +12,9 @@ def level_handler():
 def test_get_level(level_handler: LevelHandler):
     """Test that a level is correctly returned."""
     assert level_handler.get_level(1).get("name") == "Level 1"
+
+
+def test_get_map(level_handler: LevelHandler):
+    """Test that a map is correctly returned."""
+    assert len(level_handler.get_level(1).get("map")) == 31
+    assert len(level_handler.get_level(1).get("map")[0]) == 28
