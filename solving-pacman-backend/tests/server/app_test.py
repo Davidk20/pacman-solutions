ea164cc946d67bdcd870b35351be09d0e5863c39
David Kidd <zhac086@live.rhul.ac.uk>
2023-11-07 00:34:44 +0000

    created test to check for valid return of game board from server

--- solving-pacman-backend/tests/server/app_test.py
+++ solving-pacman-backend/tests/server/app_test.py
@@ -5,3 +5,9 @@ def test_app(client):
     """Basic test to check functionality of root page."""
     response = client.get("/")
     assert b"<h1 style='color:blue'>Hello There!</h1>" in response.data
+
+
+def test_get_board(client):
+    """Test that the route can return a game board."""
+    response = client.get("/get-board?level_num=1")
+    assert b"Level 1" in response.data
