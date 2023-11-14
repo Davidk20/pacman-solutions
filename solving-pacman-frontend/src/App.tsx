ac56779f92935bb6007fd77e64ba9e65999014b6
David Kidd <zhac086@live.rhul.ac.uk>
2023-11-14 23:34:20 +0000

    standardised css class names

--- solving-pacman-frontend/src/App.tsx
+++ solving-pacman-frontend/src/App.tsx
@@ -4,8 +4,8 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1 className="PageTitle">PACMAN SOLUTIONS</h1>
+      <header className="App-Header">
+        <h1 className="Page-Title">PACMAN SOLUTIONS</h1>
       </header>
     </div>
   );
