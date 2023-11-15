f32a301691bfa78ee0d0089bf98b85c45a83c482
David Kidd <zhac086@live.rhul.ac.uk>
2023-11-15 00:04:43 +0000

    added pacman logo to homepage

--- solving-pacman-frontend/src/App.tsx
+++ solving-pacman-frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import logo from "./assets/images/pacman_logo.png";
 
 function App() {
   return (
@@ -7,6 +8,9 @@ function App() {
       <header className="App-Header">
         <h1 className="Page-Title">PACMAN SOLUTIONS</h1>
       </header>
+      <div className="Home-Menu">
+        <img className="Pacman-Logo" src={logo}/>
+      </div>
     </div>
   );
 }
