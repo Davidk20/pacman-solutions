76f6cf58369fc05feec80bb089448ee82fb2d4b6
David Kidd <zhac086@live.rhul.ac.uk>
2023-11-15 01:14:20 +0000

    created constants.tsx to store constant variables between environments

---
+++ solving-pacman-frontend/src/constants.tsx
@@ -0,0 +1,22 @@
+// Constant variables which should be accessible dependent on their environment
+
+/**
+ * Environment variables for the development environment.
+ */
+const dev = {
+  url: {
+    API_URL: "http://127.0.0.1:1000"
+  }
+};
+
+/**
+* Environment variables for the production environment.
+*/
+const prod = {
+  url: {
+    // TODO change when deployed to a remote server.
+    API_URL: "http://127.0.0.1:1000"
+  }
+};
+
+export const config = process.env.NODE_ENV === "development" ? dev : prod;
