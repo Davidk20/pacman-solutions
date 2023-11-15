94e9c1a2ec6679d34007fc185ca0bd8e23e41fb2
David Kidd <zhac086@live.rhul.ac.uk>
2023-11-15 00:53:51 +0000

    refactored app.tsx to become the route home for the app

--- solving-pacman-frontend/src/App.tsx
+++ solving-pacman-frontend/src/App.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 import "./App.css";
+import { Route, HashRouter as Router, Routes } from "react-router-dom";
+import { LevelSelectionView } from "./views/level-selction-view";
 import { HomePageView } from "./views/home-page-view";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-Header">
-        <h1 className="Page-Title">PACMAN SOLUTIONS</h1>
-      </header>
-      <HomePageView></HomePageView>
-    </div>
-  );
+class App extends React.Component {
+  override render(): React.ReactNode {
+    return (
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/" Component={HomePageView} />
+            <Route path="/level-select" Component={LevelSelectionView} />
+          </Routes>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
