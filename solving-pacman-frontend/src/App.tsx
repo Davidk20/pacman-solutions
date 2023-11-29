a9c87e64512aefb15e2620182086f585d033f64c
David Kidd <zhac086@live.rhul.ac.uk>
2023-11-29 16:21:18 +0000

    added game view to routes

--- solving-pacman-frontend/src/App.tsx
+++ solving-pacman-frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { Route, HashRouter as Router, Routes } from "react-router-dom";
 import { LevelSelectionView } from "./views/level-selection-view";
 import { HomePageView } from "./views/home-page-view";
 import { ConfigurationView } from "./views/configuration-view";
+import { GameView } from "./views/game-view";
 
 class App extends React.Component {
   override render(): React.ReactNode {
@@ -14,6 +15,7 @@ class App extends React.Component {
             <Route path="/" Component={HomePageView} />
             <Route path="/level-select" Component={LevelSelectionView} />
             <Route path="/configuration" Component={ConfigurationView} />
+            <Route path="/game" Component={GameView}/>
           </Routes>
         </div>
       </Router>
