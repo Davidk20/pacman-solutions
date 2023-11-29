ce0573716de74b10bedd649bd1a7d77300ac9728
David Kidd <zhac086@live.rhul.ac.uk>
2023-11-29 19:39:36 +0000

    added fetchLevel function

--- solving-pacman-frontend/src/services/api-service.tsx
+++ solving-pacman-frontend/src/services/api-service.tsx
@@ -14,3 +14,15 @@ export async function fetchOverview(): Promise<string[]> {
   const response = await fetch(queryUrl);
   return response.json();
 }
+
+
+/**
+ * Fetch a single level, provided the level number.
+ * @param levelNum The level being requested.
+ */
+export async function fetchLevel(levelNum: number): Promise<Array<Array<number>>> {
+  const api_url = config.url.API_URL;
+  const queryUrl = `${api_url}/get-board?level_num=${levelNum}`;
+  const response = await fetch(queryUrl);
+  return response.json();
+}
