f3c0d72800a33a5052154c1e50d7bee1d89b8dce
David Kidd <zhac086@live.rhul.ac.uk>
2023-11-29 15:21:39 +0000

    refacted level selection view to render list of levels

--- solving-pacman-frontend/src/views/level-selection-view.tsx
+++ solving-pacman-frontend/src/views/level-selection-view.tsx
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { ReactElement } from "react";
 import "../styles/components/home-menu-button.css";
 import { Header } from "../components/header";
+import { fetchOverview } from "../services/api-service";
 
 /**
  * View showing the level selection screen.
  */
 export class LevelSelectionView extends React.Component{
 
+  override state = {
+    formattedLevels: []
+  };
+
+
+  override async componentDidMount(): Promise<void> {
+    const levels = await fetchOverview();
+    const levelElements: Array<ReactElement> = [];
+    for (const level of levels){
+      levelElements.push(<span>{level}</span>);
+    }
+    this.setState({formattedLevels: levelElements});
+  }
+
   override render(): React.ReactNode {
     return (
       <div className="Page">
         <Header subtitle="LEVEL SELECTION"></Header>
-
+        {this.state.formattedLevels}
       </div>
     );
   }
