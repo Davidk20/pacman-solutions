6163a51b0f606322c3cbb6fe06fa9b3d34ff7261
David Kidd <zhac086@live.rhul.ac.uk>
2023-11-30 14:37:52 +0000

    made title a link back to the homepage

--- solving-pacman-frontend/src/components/header.tsx
+++ solving-pacman-frontend/src/components/header.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../styles/components/header.scss";
+import { Link } from "react-router-dom";
 
 /**
  * Props for Header.
@@ -16,7 +17,9 @@ export class Header extends React.Component<HeaderProps>{
   override render(): React.ReactNode {
     return (
       <header className="App-Header">
-        <h1 className="Page-Title">PACMAN SOLUTIONS</h1>
+        <Link to="/" style={{textDecoration: "none"}}>
+          <h1 className="Page-Title">PACMAN SOLUTIONS</h1>
+        </Link>
         <h2 className="Page-Subtitle">{this.props.subtitle.toUpperCase()}</h2>
       </header>
     );
