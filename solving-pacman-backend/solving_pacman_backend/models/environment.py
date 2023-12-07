5950f37af081861252225ef69404e95484602cd5
David Kidd <zhac086@live.rhul.ac.uk>
2023-12-07 17:05:30 +0000

    created entities for the environment

---
+++ solving-pacman-backend/solving_pacman_backend/models/environment.py
@@ -0,0 +1,30 @@
+"""Models representing the environment."""
+
+
+class EnvironmentEntity:
+    """Parent class representing a generic environment entity."""
+
+    def __init__(self) -> None:
+        """Initialise the class."""
+        self.value = 999
+        """
+        The value held by this item in the Array representation
+        """
+
+
+class Wall(EnvironmentEntity):
+    """Model representing a Wall."""
+
+    def __init__(self) -> None:
+        """Initialise the class."""
+        super().__init__()
+        self.value = 99
+
+
+class Gate(EnvironmentEntity):
+    """Model representing the gate between the ghost spawn and the map."""
+
+    def __init__(self) -> None:
+        """Initialise the class."""
+        super().__init__()
+        self.value = 20
