a4f5819f9e5356304ea76a0e2c2fdac14b8ed38c
David Kidd <zhac086@live.rhul.ac.uk>
2023-12-07 17:09:15 +0000

    made Node entity attribute more generalisable

--- solving-pacman-backend/solving_pacman_backend/models/node.py
+++ solving-pacman-backend/solving_pacman_backend/models/node.py
@@ -1,5 +1,6 @@
 """Model representing a node within a graph."""
 from solving_pacman_backend.models.agent import Agent
+from solving_pacman_backend.models.environment import EnvironmentEntity
 from solving_pacman_backend.models.pickups import Pickup
 
 
@@ -13,7 +14,7 @@ class Node:
     """
 
     def __init__(
-        self, position: tuple[int, int], agent: Agent = None, pickup: Pickup = None
+        self, position: tuple[int, int], entity: Agent | Pickup | EnvironmentEntity
     ) -> None:
         """
         Initialise a `Node` object.
@@ -24,33 +25,24 @@ class Node:
             Tuple containing the x and y coordinates of
             the node relative to the array.
 
-        `agent` : `Agent`
-            The agent which is currently in this space, `None` otherwise.
-
-        `pickup` : `Pickup`
-            The pickup which is currently in this space, `None` otherwise.
+        `entity` : `Agent | Pickup | EnvironmentEntity`
+            The entity which is currently in this space.
         """
-        if agent and pickup:
-            raise ValueError(
-                "A newly added Node cannot have both an Agent and Pickup Item."
-            )
         self.visited = False
         """`true` if the node has been visited by the Pac-Man agent."""
         self.position = position
         """The position of the node in relation to the array-based level."""
-        self.agent = agent
-        """Stores the agent currently within this space. `None` otherwise."""
-        self.pickup = pickup
-        """Stores the item currently within this space. `None` otherwise."""
+        self.entity = entity
+        """Stores the entity currently within this space."""
 
     def __str__(self) -> str:
         return f"""
             Position: {self.position},
-            Contains: {self.agent if self.agent is not None else self.pickup}
+            Contains: {self.entity}
         """
 
     def __repr__(self) -> str:
         return f"""
             Position: {self.position},
-            Contains: {self.agent if self.agent is not None else self.pickup}
+            Contains: {self.entity}
         """
