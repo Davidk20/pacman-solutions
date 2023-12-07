ac61ed2880dcb2b52827788a56c112e922984c2c
David Kidd <zhac086@live.rhul.ac.uk>
2023-12-07 14:09:28 +0000

    added functionality to map graph nodes to edges

--- solving-pacman-backend/solving_pacman_backend/models/graph.py
+++ solving-pacman-backend/solving_pacman_backend/models/graph.py
@@ -15,6 +15,36 @@ class Graph:
         self.node_count = 1
         """The counter used to identify nodes."""
 
+    def size(self) -> int:
+        """
+        Returns the size of the graph.
+
+        Returns
+        -------
+        The number of nodes within the graph.
+        """
+        return len(self.level)
+
     def add_node(self, node: Node) -> None:
+        """
+        Adds a single, unconnected `Node` into the graph.
+
+        Parameters
+        ----------
+        `node` : `Node`
+            The `Node` object to add to the graph.
+        """
         self.level.add_nodes_from([(self.node_count, {"position": node.position})])
         self.node_count += 1
+
+    def map_edges(self, mapping: dict[tuple[int, int], list[tuple[int, int]]]) -> None:
+        """
+        Connects all nodes together using the
+        """
+        # TODO should check here that graph is fully connected or throw error
+        node_pos = list(mapping.keys())
+        for pos, connections in mapping.items():
+            parent = node_pos.index(pos) + 1
+            for connection in connections:
+                child = node_pos.index(connection) + 1
+                self.level.add_edge(parent, child)
--- solving-pacman-backend/tests/models/graph_test.py
+++ solving-pacman-backend/tests/models/graph_test.py
@@ -18,6 +18,26 @@ def node():
 
 
 def test_add_node(graph: Graph, node: Node):
+    """Test that a node is correctly added to the graph."""
     graph.add_node(node)
-    assert len(graph.level.nodes) == 1
+    assert graph.size() == 1
     assert graph.node_count == 2
+
+
+def test_map_edges(graph: Graph):
+    """Test that edges are correctly mapped using the adjacency matrix."""
+    nodes = [Node((0, 0)), Node((0, 1)), Node((0, 2)), Node((0, 3))]
+
+    adjacency_list: dict[tuple[int, int], list[tuple[int, int]]] = {
+        (0, 0): [(0, 1), (0, 2)],
+        (0, 1): [(0, 3), (0, 2)],
+        (0, 2): [(0, 0), (0, 0)],
+        (0, 3): [(0, 1), (0, 2)],
+    }
+
+    for node in nodes:
+        graph.add_node(node)
+
+    graph.map_edges(adjacency_list)
+
+    assert len(graph.level.edges([1])) == 2
