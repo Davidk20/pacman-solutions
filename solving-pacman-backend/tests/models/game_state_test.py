ba2bf91e75f5984d2f48f710ad248d7e0428858f
David Kidd <zhac086@live.rhul.ac.uk>
2024-01-16 01:24:56 +0000

    deleted game_state_test
    As game_state is now a storage model, it has no functions to be tested.

--- solving-pacman-backend/tests/models/game_state_test.py
+++
@@ -1,9 +0,0 @@
-"""Tests for the GameState model."""
-from solving_pacman_backend.models.game_state import GameState
-
-
-def test_timer_increment():
-    """Test that the timer correctly increments."""
-    gameState = GameState()
-    gameState.increment_timer()
-    assert gameState.timer == 1
