bad62ead23e31ce2d73575f75f40574b6c603a2c
David Kidd <zhac086@live.rhul.ac.uk>
2024-01-16 14:29:18 +0000

    implemented graph to array conversion

--- solving-pacman-backend/solving_pacman_backend/utils/level_utils.py
+++ solving-pacman-backend/solving_pacman_backend/utils/level_utils.py
@@ -1,6 +1,7 @@
 """Generic functions to be applied to instances of Levels."""
 from solving_pacman_backend import constants
 from solving_pacman_backend.models.graph import Graph
+from solving_pacman_backend.models.graph import NodeNotFoundException
 
 
 def print_level(level: list[list[int]]) -> None:
@@ -22,5 +23,14 @@ def graph_to_array(graph: Graph) -> list[list[int]]:
     -------
     A 2-D list containing the level.
     """
-    level = [[99] * constants.PACMAN_BOARD_WIDTH] * constants.PACMAN_BOARD_HEIGHT
+    level = []
+    for row in range(constants.PACMAN_BOARD_HEIGHT):
+        level.append([])
+        for column in range(constants.PACMAN_BOARD_WIDTH):
+            try:
+                node = graph.find_node_by_pos((column, row))
+                level[row].append(node.entity.value)
+            except NodeNotFoundException:
+                level[row].append(99)
+                continue
     return level
--- solving-pacman-backend/tests/utils/level_utils_test.py
+++ solving-pacman-backend/tests/utils/level_utils_test.py
@@ -1,4 +1,5 @@
 """Tests on the level utility functions."""
+from solving_pacman_backend.services.level_handler import LevelHandler
 from solving_pacman_backend.utils.level_utils import graph_to_array
 from tests import mocks_test
 
@@ -12,3 +13,9 @@ def test_graph_to_array_size():
     array = graph_to_array(graph)
     assert len(array) == 31
     assert len(array[0]) == 28
+
+
+def test_graph_to_array_conversion():
+    """Test that the conversion is valid."""
+    level_handler = LevelHandler()
+    assert graph_to_array(mocks_test.mock_graph()) == level_handler.get_map(1)
