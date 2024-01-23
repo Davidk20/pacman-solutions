29d66e14e7d524a3231aa2cf17e1cee7cb379448
David Kidd <zhac086@live.rhul.ac.uk>
2024-01-23 16:12:58 +0000

    refactored agent to be an abstract class

--- solving-pacman-backend/solving_pacman_backend/models/agent.py
+++ solving-pacman-backend/solving_pacman_backend/models/agent.py
@@ -1,8 +1,24 @@
-"""Abstract class used to encapsulate all agent attributes."""
+"""
+Abstract class used to encapsulate all agent attributes.
+
+Inspiration for this Agent model is being taken from the CS3940 Intelligent
+Agents VacuumWorld abstraction. The core functionality of an `Agent` should
+follow the agent cycle:
+
+1) Perceive environment state and generate percept through `see()`
+2) Revise internal state through `next()`
+3) decide which action to select through `action()`
+4) Return the action to be executed by the body
+
+https://github.com/dicelab-rhul/vacuumworld
+"""
+from abc import ABC
+from abc import abstractmethod
+
 from solving_pacman_backend.models.movement_types import MovementTypes
 
 
-class Agent:
+class Agent(ABC):
     """Abstract class used to encapsulate all agent attributes."""
 
     def __init__(self):
@@ -26,3 +42,26 @@ class Agent:
 
     def __repr__(self) -> str:
         return f"""(Name: {self.name}, Value: {self.value})"""
+
+    @abstractmethod
+    def perceive(self) -> None:
+        """Perceive the environment and generate perceptions."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def revise(self) -> None:
+        """Revise internal states using the perceptions."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def execute(self) -> tuple[int, int]:
+        """
+        Returns the position which the `Agent` should move to.
+
+        Uses the perceptions and revisions to decide the best action to take.
+
+        Returns
+        -------
+        A `tuple` containing the coordinates for the agent to move to.
+        """
+        raise NotImplementedError
