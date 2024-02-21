05026524c9a3cdeb59f5d51e5ddada0919da49da
David Kidd <zhac086@live.rhul.ac.uk>
2024-02-21 23:26:40 +0000

    created exceptions file to store exceptions

---
+++ solving-pacman-backend/solving_pacman_backend/exceptions.py
@@ -0,0 +1,18 @@
+"""Centralised file for all exceptions."""
+##########################################
+#            Level Exceptions
+##########################################
+
+
+class LevelNotFoundException(Exception):
+    """Raised when a level is not found."""
+
+    def __init__(self, level_num: int) -> None:
+        super().__init__(f"Level {level_num} not found.")
+
+
+class IncorrectLevelConfigurationException(Exception):
+    """Raised when a level is not configured correctly."""
+
+    def __init__(self, level_num: int) -> None:
+        super().__init__(f"Level {level_num} is not configured correctly.")
