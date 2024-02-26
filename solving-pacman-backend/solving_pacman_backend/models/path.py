c477dc5e249dafe703f9101798dfcac232c1e082
David Kidd <zhac086@live.rhul.ac.uk>
2024-02-26 15:15:55 +0000

    added magic methods

--- solving-pacman-backend/solving_pacman_backend/models/path.py
+++ solving-pacman-backend/solving_pacman_backend/models/path.py
@@ -2,7 +2,18 @@ from solving_pacman_backend.models.node import Node
 
 
 class Path:
-    """Model representing a single path."""
+    """
+    Model representing a single path.
+
+    This object should be read-only, and acts as a wrapper to allow
+    functions to take place on `Path`'s.
+    """
 
     def __init__(self, path: list[Node]) -> None:
         self.path = path
+
+    def __repr__(self) -> str:
+        return f"Path from {self.path[0].position} to {self.path[-1].position}"
+
+    def __len__(self) -> int:
+        return len(self.path)
