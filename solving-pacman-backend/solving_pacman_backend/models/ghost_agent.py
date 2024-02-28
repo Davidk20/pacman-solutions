16413140d6218602db83d5b27f52e87dc4cc42b1
David Kidd <zhac086@live.rhul.ac.uk>
2024-02-28 02:41:00 +0000

    implemented basic logic for first ghost Blinky
    This is the basic logic required for this single ghost to chase
    Pac-Man. In this behaviour, it will constantly optimise its path
    to chase Pac-Man as efficiently as possible.
    
    In future, this behaviour will be integrated into a single
    'GhostAgent' class from which the ghosts can be created

--- solving-pacman-backend/solving_pacman_backend/models/ghost_agent.py
+++ solving-pacman-backend/solving_pacman_backend/models/ghost_agent.py
@@ -2,6 +2,9 @@
 from solving_pacman_backend.models.agent import Agent
 from solving_pacman_backend.models.game_state import GameState
 from solving_pacman_backend.models.movement_types import MovementTypes
+from solving_pacman_backend.models.pacman_agent import PacmanAgent
+from solving_pacman_backend.models.path import Path
+from solving_pacman_backend.utils import level_utils
 
 
 class BlinkyAgent(Agent):
@@ -19,10 +22,27 @@ class BlinkyAgent(Agent):
         super().__init__("Blinky", "Shadow", MovementTypes.CHASE, home_path, 21, 200)
 
     def _perceive(self, state: GameState) -> None:
-        raise NotImplementedError
+        graph = level_utils.array_to_graph(state.board_state)
+        # When returning an agent it should return a single item
+        pacman_node = graph.find_node_by_entity(PacmanAgent)[0]
+        self.path: Path = Path([])
+        match self.movement_type:
+            case MovementTypes.CHASE:
+                # If chasing Pac-Man, this should be the only target.
+                self.target = [pacman_node.position]
+                self.path = graph.shortest_path_to(self.position, self.target[0])
+                # The path contains the current pos which must be popped from the list
+                self.path.get_next_pos()
+                # print(self.path.route)
+            case MovementTypes.SCATTER:
+                # When scattering to home, this should become their target.
+                self.target = self.home_path
+            case _:
+                # All other conditions should clear the target.
+                self.target = []
 
-    def _execute(self):
-        raise NotImplementedError
+    def _execute(self) -> tuple[int, int]:
+        return self.path.get_next_pos().position
 
 
 class PinkyAgent(Agent):
