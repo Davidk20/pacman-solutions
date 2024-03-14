c76b9ded65777716d06ff98de3f2e607a74928f5
David Kidd <zhac086@live.rhul.ac.uk>
2024-03-14 13:05:09 +0000

    refactored level_utils to use new Entity format

--- solving-pacman-backend/solving_pacman_backend/utils/level_utils.py
+++ solving-pacman-backend/solving_pacman_backend/utils/level_utils.py
@@ -91,7 +91,7 @@ def graph_to_array(graph: Graph) -> list[list[int]]:
         for column in range(constants.PACMAN_BOARD_WIDTH):
             try:
                 node = graph.find_node_by_pos((column, row))
-                level[row].append(node.entity.value)
+                level[row].append(node.entity.value())
             except NodeNotFoundException:
                 level[row].append(99)
                 continue
