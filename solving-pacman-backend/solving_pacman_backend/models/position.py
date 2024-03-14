d3f7bb8d3ff70af20f2f1792ad00980c4fed13b4
David Kidd <zhac086@live.rhul.ac.uk>
2024-03-14 23:16:32 +0000

    created model for position

---
+++ solving-pacman-backend/solving_pacman_backend/models/position.py
@@ -0,0 +1,11 @@
+"""Model representing a position in two-dimensional space."""
+
+
+class Position:
+    """Model representing a position in two-dimensional space."""
+
+    def __init__(self, x: int, y: int) -> None:
+        self.x = x
+        """The x-coordinate."""
+        self.y = y
+        """The y-coordinate."""
