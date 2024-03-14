890d3b3147d41970ea16a62696fcb04571a18b20
David Kidd <zhac086@live.rhul.ac.uk>
2024-03-14 22:03:44 +0000

    refactored ghost_agent perception to interpret graph

--- solving-pacman-backend/solving_pacman_backend/models/ghost_agent.py
+++ solving-pacman-backend/solving_pacman_backend/models/ghost_agent.py
@@ -1,10 +1,9 @@
 """Collection of models representing the Ghosts."""
 from solving_pacman_backend.models.agent import Agent
-from solving_pacman_backend.models.data_types import AgentHomes
+from solving_pacman_backend.models.graph import Graph
 from solving_pacman_backend.models.movement_types import MovementTypes
 from solving_pacman_backend.models.pacman_agent import PacmanAgent
 from solving_pacman_backend.models.path import Path
-from solving_pacman_backend.utils import level_utils
 
 
 class GhostAgent(Agent):
@@ -29,16 +28,15 @@ class GhostAgent(Agent):
     ):
         super().__init__(name, behaviour, movement_type, home_path, value, score)
 
-    def _perceive(self, time: int, level: list[list[int]]) -> None:
-        graph = level_utils.array_to_graph(level)
+    def _perceive(self, time: int, level: Graph) -> None:
         # When returning an agent it should return a single item
-        pacman_node = graph.find_node_by_entity(PacmanAgent)[0]
+        pacman_node = level.find_node_by_entity(PacmanAgent)[0]
         self.path: Path = Path([])
         match self.movement_type:
             case MovementTypes.CHASE:
                 # If chasing Pac-Man, this should be the only target.
                 self.target = [pacman_node.position]
-                self.path = graph.shortest_path_to(self.position, self.target[0])
+                self.path = level.shortest_path_to(self.position, self.target[0])
                 # The path contains the current pos which must be popped from the list
                 self.path.get_next_pos()
             case MovementTypes.SCATTER:
@@ -49,13 +47,8 @@ class GhostAgent(Agent):
                 self.target = []
 
     def _execute(self) -> tuple[int, int]:
-        return self.path.get_next_pos().position
-
-
-def gen_all_ghosts(homes: AgentHomes) -> list[GhostAgent]:
-    return [
-        GhostAgent("Blinky", "Shadow", MovementTypes.CHASE, homes["blinky"], 21, 200),
-        GhostAgent("Pinky", "Speedy", MovementTypes.HOMEBOUND, homes["pinky"], 22, 200),
-        GhostAgent("Inky", "Bashful", MovementTypes.HOMEBOUND, homes["inky"], 23, 200),
-        GhostAgent("Clyde", "Pokey", MovementTypes.HOMEBOUND, homes["clyde"], 24, 200),
-    ]
+        match self.movement_type:
+            case MovementTypes.CHASE:
+                return self.path.get_next_pos().position
+            case _:
+                return self.position
