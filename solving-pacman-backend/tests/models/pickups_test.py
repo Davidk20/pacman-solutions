d37ac35ffcbed5b0f29ef3c673285c3b2f5be056
David Kidd <zhac086@live.rhul.ac.uk>
2024-03-14 11:18:02 +0000

    refactored tests to match new entity model

--- solving-pacman-backend/tests/models/pickups_test.py
+++ solving-pacman-backend/tests/models/pickups_test.py
@@ -13,13 +13,13 @@ from solving_pacman_backend.models.pickups import Strawberry
 
 def test_get_score():
     """Tests that the items all have the correct score"""
-    assert PacDot().get_score() == 10
-    assert PowerPellet().get_score() == 50
-    assert Cherry().get_score() == 100
-    assert Strawberry().get_score() == 300
-    assert Orange().get_score() == 500
-    assert Apple().get_score() == 700
-    assert Melon().get_score() == 1000
-    assert Galaxian().get_score() == 2000
-    assert Bell().get_score() == 3000
-    assert Key().get_score() == 5000
+    assert PacDot().score() == 10
+    assert PowerPellet().score() == 50
+    assert Cherry().score() == 100
+    assert Strawberry().score() == 300
+    assert Orange().score() == 500
+    assert Apple().score() == 700
+    assert Melon().score() == 1000
+    assert Galaxian().score() == 2000
+    assert Bell().score() == 3000
+    assert Key().score() == 5000
