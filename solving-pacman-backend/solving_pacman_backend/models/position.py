4b8ea4c10d37d7fc0e6e69beb380363e8100f11c
David Kidd <zhac086@live.rhul.ac.uk>
2024-03-14 23:29:08 +0000

    added Euclidean distance function to position

--- solving-pacman-backend/solving_pacman_backend/models/position.py
+++ solving-pacman-backend/solving_pacman_backend/models/position.py
@@ -9,3 +9,16 @@ class Position:
         """The x-coordinate."""
         self.y = y
         """The y-coordinate."""
+
+    def distance(self, other) -> float:
+        """
+        Calculates the distance between this position and the `other` position
+        using the Euclidean Distance.
+
+        Parameters
+        ----------
+        `other` : `Position`
+            The other position to calculate the distance to,
+            rounded to 4 decimal places.
+        """
+        return round(((self.x - other.x) ** 2 + (self.y - other.y) ** 2) ** 0.5, 4)
