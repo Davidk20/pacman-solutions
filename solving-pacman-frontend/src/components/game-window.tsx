87530fc787a1515059531b7e761b81fe711a2ead
David Kidd <zhac086@live.rhul.ac.uk>
2024-03-15 13:33:59 +0000

    refactored GameWindow to use new div-based components

--- solving-pacman-frontend/src/components/game-window.tsx
+++ solving-pacman-frontend/src/components/game-window.tsx
@@ -3,82 +3,100 @@
  * https://codesandbox.io/p/sandbox/canvas-react-typescript-lqq9k?file=%2Fsrc%2FApp.tsx
  */
 
-import React, { useEffect, useRef } from "react";
-import Wall from "./canvas/wall";
-import Dot from "./canvas/dot";
-import PowerPellet from "./canvas/power-pellet";
-import Agent from "./canvas/agent";
+import React from "react";
+import { Wall } from "./game/wall";
+import { Dot } from "./game/dot";
+import { Energiser } from "./game/energiser";
 
 /**
- * Function to return a game window.
- *
- * @param level The level as a 2-D array.
+ * Props for the Game Window.
  */
-export default function GameWindow( {level}: Readonly<{level: number[][]}> ) {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  const frameRef = useRef<number>(0);
-  const height = 800;
-  const width = 800;
-  let xPos = 0;
-  let yPos = 0;
+type GameWindowProps = {
+  /**
+   * The level.
+   */
+  level: number[][]
+}
 
-  function draw(context: CanvasRenderingContext2D) {
-    if (context) {
-      // Draw base game window
-      context.fillStyle = "black";
-      context.fillRect(0, 0, height, width);
-      // Draw game
-      for (const row of level) {
-        for (const pos of row) {
-          switch (pos) {
-          case 1:
-            Dot(context, height, xPos, yPos);
-            break;
-          case 2:
-            PowerPellet(context, height, xPos, yPos);
-            break;
-          case 21:
-            Agent(context, "blinky", height, xPos, yPos);
-            break;
-          case 22:
-            Agent(context, "clyde", height, xPos, yPos);
-            break;
-          case 23:
-            Agent(context, "inky", height, xPos, yPos);
-            break;
-          case 24:
-            Agent(context, "pinky", height, xPos, yPos);
-            break;
-          case 44:
-            Agent(context, "pacman", height, xPos, yPos);
-            break;
-          case 99:
-            Wall(context, height, xPos, yPos);
-            break;
-          default:
-            break;
-          }
-          xPos = xPos + (width / 28);
+/**
+ * Component to return the game window from which the user can see the game..
+ */
+export default class GameWindow extends React.Component<GameWindowProps> {
+  entityHeight = 22;
+  /**
+   * The height of a single entity within the game.
+   */
+  entityWidth = 22;
+  /**
+   * The width of a single entity within the game.
+   */
+  windowHeight: number = 0;
+  /**
+   * The height of the game window.
+   */
+  windowWidth: number = 0;
+  /**
+   * The width of the game window.
+   */
+
+
+  override render(): React.ReactNode {
+    this.windowHeight = this.entityHeight * this.props.level.length;
+
+    const gameComponents = [];
+    for (let row = 0; row < this.props.level.length; row++) {
+      const componentRow = [];
+      for (let col = 0; col < this.props.level[row].length; col++) {
+        // level[0] can only be read whilst accessing props.
+        this.windowWidth = this.entityWidth * this.props.level[0].length;
+        if (this.props.level[row][col] == 99) {
+          componentRow.push(
+            <Wall
+              key={col*row*Math.random()}
+              width={this.entityWidth}
+              height={this.entityHeight}
+              yPos={this.entityHeight*row}
+              xPos={this.entityWidth*col}
+            />
+          );
         }
-        xPos = 0;
-        yPos = yPos + (height / 31);
+        if (this.props.level[row][col] == 1) {
+          componentRow.push(
+            <Dot
+              key={col*row*Math.random()}
+              width={this.entityWidth}
+              height={this.entityHeight}
+              yPos={this.entityHeight*row}
+              xPos={this.entityWidth*col}
+            />
+          );
+        }
+        if (this.props.level[row][col] == 2) {
+          componentRow.push(
+            <Energiser
+              key={col*row*Math.random()}
+              width={this.entityWidth}
+              height={this.entityHeight}
+              yPos={this.entityHeight*row}
+              xPos={this.entityWidth*col}
+            />
+          );
+        }
       }
+      gameComponents.push(componentRow);
     }
+
+    return (
+      <div
+        style={{
+          height: this.windowHeight,
+          width: this.windowWidth,
+          position: "absolute",
+          backgroundColor: "black"
+        }}
+      >
+        {gameComponents}
+      </div>
+    );
   }
-
-  useEffect(() => {
-    if (canvasRef.current) {
-      const context = canvasRef.current.getContext("2d");
-
-      if (context) {
-        context.canvas.height = height;
-        context.canvas.width = width;
-
-        frameRef.current = requestAnimationFrame(() => draw(context));
-      }
-    }
-    return () => cancelAnimationFrame(frameRef.current);
-  }, [height, width]);
-
-  return <canvas ref={canvasRef} />;
 }
