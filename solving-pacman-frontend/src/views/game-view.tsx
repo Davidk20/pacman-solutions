970da2ebaa67fa2e6f0d9ab64b83f829aa9d3ebb
David Kidd <zhac086@live.rhul.ac.uk>
2024-03-18 16:10:37 +0000

    Refactored game-view loading
    - Implemented ldrs package for loading animation rather than basic text
    - Used ternary operator instead of duplicating code

--- solving-pacman-frontend/src/views/game-view.tsx
+++ solving-pacman-frontend/src/views/game-view.tsx
@@ -5,6 +5,7 @@ import GameWindow from "../components/game-window";
 import "../styles/views/game-view.scss";
 import { fetchGame } from "../services/api-service";
 import { GameState } from "@models/game-state";
+import { tailChase } from "ldrs";
 
 /**
  * View showing the render of the game window.
@@ -13,6 +14,8 @@ export function GameView() {
   const location = useLocation();
   const level_num = location.state.level;
   const [game, setGame] = useState<GameState[]>();
+
+  tailChase.register();
 
   /**
    * Fetches the game data from the API service.
@@ -30,26 +33,12 @@ export function GameView() {
       .catch(console.error);
   }, []);
 
-  if (game) {
-    console.log(game);
-    return (
-      <div className="Page">
-        <Header subtitle={level_num}></Header>
-        <div className="Game-Container">
-          <GameWindow level={game[0].state} ></GameWindow>
-        </div>
-
+  return (
+    <div className="Page">
+      <Header subtitle={level_num}></Header>
+      <div className="Game-Container">
+        {game ? <GameWindow level={game[0].state} ></GameWindow> : <l-tail-chase size="150" speed="2.5" color="#F4D147"></l-tail-chase>}
       </div>
-    );
-  }
-  else {
-    return (
-      <div className="Page">
-        <Header subtitle={level_num}></Header>
-        <div className="Game-Container">
-          <p>loading...</p>
-        </div>
-      </div>
-    );
-  }
+    </div>
+  );
 }
