c602d42ba527c24462c73bc6aa4351ecf8f85ec8
David Kidd <zhac086@live.rhul.ac.uk>
2024-03-18 14:58:33 +0000

    refactored game-view to interpret full game state

--- solving-pacman-frontend/src/views/game-view.tsx
+++ solving-pacman-frontend/src/views/game-view.tsx
@@ -3,7 +3,8 @@ import { Header } from "../components/header";
 import { useLocation } from "react-router-dom";
 import GameWindow from "../components/game-window";
 import "../styles/views/game-view.scss";
-import { fetchLevel } from "../services/api-service";
+import { fetchGame } from "../services/api-service";
+import { GameState } from "@models/game-state";
 
 /**
  * View showing the render of the game window.
@@ -11,23 +12,44 @@ import { fetchLevel } from "../services/api-service";
 export function GameView() {
   const location = useLocation();
   const level_num = location.state.level;
-  const [level, setLevel] = useState<number[][]>([]);
+  const [game, setGame] = useState<GameState[]>();
+
+  /**
+   * Fetches the game data from the API service.
+   * @returns A `list` of `GameStates`
+   */
+  const fetchData = async (): Promise<GameState[]> => {
+    return await fetchGame(level_num.charAt(level_num.length-1));
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
-      setLevel(await fetchLevel(level_num.charAt(level_num.length-1)));
-      console.log("Level Loaded");
-    };
     fetchData()
+      .then(response => {
+        setGame(response);
+      })
       .catch(console.error);
   }, []);
-  return (
-    <div className="Page">
-      <Header subtitle={level_num}></Header>
-      <div className="Game-Container">
-        <GameWindow level={level} ></GameWindow>
-      </div>
 
-    </div>
-  );
+  if (game) {
+    console.log(game);
+    return (
+      <div className="Page">
+        <Header subtitle={level_num}></Header>
+        <div className="Game-Container">
+          <GameWindow level={game[0].state} ></GameWindow>
+        </div>
+
+      </div>
+    );
+  }
+  else {
+    return (
+      <div className="Page">
+        <Header subtitle={level_num}></Header>
+        <div className="Game-Container">
+          <p>loading...</p>
+        </div>
+      </div>
+    );
+  }
 }
