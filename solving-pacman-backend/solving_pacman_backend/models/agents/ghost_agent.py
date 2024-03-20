93ffc54a49e1730ffa69be502cc9e0e5ccb77243
David Kidd <zhac086@live.rhul.ac.uk>
2024-03-20 14:24:34 +0000

    refactored GhostAgent so specific ghost logic is stored in the ghost class

--- solving-pacman-backend/solving_pacman_backend/models/agents/ghost_agent.py
+++ solving-pacman-backend/solving_pacman_backend/models/agents/ghost_agent.py
@@ -58,35 +58,42 @@ class GhostAgent(Agent):
                 # Reset timer
                 self._internal_time = 0
 
-        pacman_node = level.find_node_by_entity(PacmanAgent)[0]
-        match self.movement_type:
-            case MovementTypes.CHASE:
-                # If chasing Pac-Man, this should be the only target.
-                self.target = [pacman_node.position]
-                self.path = level.shortest_path_to(self.position, self.target[0])
-                # The path contains the current pos which must be popped from the list
-                self.path.get_next_pos()
-            case MovementTypes.SCATTER:
-                # When scattering to home, this should become their target.
-                self.target = self.home_path
-            case _:
-                # All other conditions should clear the target.
-                self.target = []
+        if self.movement_type == MovementTypes.SCATTER:
+            if len(self.target) > 0 and self.position == self.target[0]:
+                self.target.pop(0)
+            self.path = level.shortest_path_to(self.position, self.target[0])
 
     def _execute(self) -> tuple[int, int]:
         match self.movement_type:
             case MovementTypes.CHASE:
+                return self.path.get_next_pos().position
+            case MovementTypes.SCATTER:
                 return self.path.get_next_pos().position
             case _:
                 return self.position
 
 
 class BlinkyAgent(GhostAgent):
+    """
+    Specific mind for Blinky the ghost.
+
+    Blinky adapts the generic `Ghost` mind by implementing a
+    direct chasing pattern. Blinky will target Pac-Man's exact
+    position.
+    """
+
     def __init__(self, homes: list[tuple[int, int]]):
         super().__init__("Blinky", "Shadow", MovementTypes.CHASE, homes, 21, 200)
 
     def _perceive(self, time: int, level: Graph) -> None:
         super()._perceive(time, level)
+        if self.movement_type == MovementTypes.CHASE:
+            pacman_node = level.find_node_by_entity(PacmanAgent)[0]
+            self.target = [pacman_node.position]
+            self.path = level.shortest_path_to(self.position, self.target[0])
+        if self.path.route[0] == self.position:
+            # The path contains the current pos which must be popped from the list
+            self.path.get_next_pos()
 
 
 class PinkyAgent(GhostAgent):
