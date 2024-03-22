bf59ff9b35c411075b36f16779027b6f85d3f568
David Kidd <zhac086@live.rhul.ac.uk>
2024-03-22 14:53:52 +0000

    refactored iteration of InformedPacMan

--- solving-pacman-backend/solving_pacman_backend/models/agents/custom_agents/random_informed.py
+++ solving-pacman-backend/solving_pacman_backend/models/agents/custom_agents/random_informed.py
@@ -1,6 +1,9 @@
 """Model representing the informed randomised Pac-Man behaviour"""
+from random import choice
+
 from solving_pacman_backend.models.agents.pacman_agent import PacmanAgent
 from solving_pacman_backend.models.graph import Graph
+from solving_pacman_backend.models.path import Path
 
 
 class RandomInformedPacMan(PacmanAgent):
@@ -18,26 +21,42 @@ class RandomInformedPacMan(PacmanAgent):
         super().__init__(home_path, respawn_point)
 
     def _perceive(self, time: int, level: Graph) -> None:
-        # Remove target if agent has reached position
-        if len(self.target) > 0 and self.position == self.target[0]:
-            self.target.pop(0)
-        # If there is no target, assign new target
-        if len(self.target) == 0:
-            self.target.append(level.random_node().position)
-        # Find path to target
-        self.path = level.shortest_path_to(self.position, self.target[0])
-        while not self.path.is_safe():
-            # Looks for a new path every time the path is deemed unsafe.
-            self.target[0] = level.random_node().position
-            self.path = level.shortest_path_to(self.position, self.target[0])
+        current_node = level.find_node_by_pos(self.position)
+        if (
+            not level.is_junction(current_node, self.move_history[-1])
+            and self.path.is_safe()
+            and len(self.path) > 0
+        ):
+            # If the current path is valid then stay on this path
+            return
 
-        if len(self.move_history) > 0:
-            # If the agent has a move history, check that the agent
-            # is not moving backwards
-            while self.path.backwards(self.move_history):
-                self.target.pop(0)
-                self.target.append(level.random_node().position)
-                self.path = level.shortest_path_to(self.position, self.target[0])
+        # Get all paths to next jct
+        paths = level.find_path_to_next_jct(self.position)
+        # Find all safe paths
+        valid_paths: list[Path] = [path for path in paths if path.is_safe()]
+        # prune paths where the path only contains the target.
+        valid_paths = [path for path in valid_paths if len(paths) > 2]
+        # prune paths where the end point == starting point
+        valid_paths = [path for path in valid_paths if not path.is_loop()]
+        backwards_paths = valid_paths
+        if len(self.move_history) > 1:
+            # Remove any paths which would take the agent backwards
+            valid_paths = [
+                path for path in valid_paths if path.route[0] != self.move_history[-1]
+            ]
+
+        # choose a safe path to follow
+        if len(valid_paths) > 0:
+            self.path = choice(valid_paths)
+        # If no safe paths exist, allow backwards paths.
+        elif len(backwards_paths) > 0:
+            print("using this")
+            self.path = choice(backwards_paths)
+        else:
+            # If no safe path is found, choose the best scoring path
+            # so that highest score can be obtained before death.
+            sorted_paths = sorted(paths, key=lambda path: path.cost(), reverse=True)
+            self.path: Path = sorted_paths[0]
         # remove current pos from path to prevent static glitch
         self.path.get_next_pos()
 
