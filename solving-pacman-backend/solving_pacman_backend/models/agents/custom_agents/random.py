b5d4a28bb8763bf4f3d6189a0245b86898a5b6e0
David Kidd <davidkidd.dev@gmail.com>
2024-03-23 00:26:58 +0000

    Fixed random agent's occasional crashing
    - By using a similar random path model to a frightened ghost, I
      was better able to implement a random Pac-Man agent which did not
      have the same bugs.

--- solving-pacman-backend/solving_pacman_backend/models/agents/custom_agents/random.py
+++ solving-pacman-backend/solving_pacman_backend/models/agents/custom_agents/random.py
@@ -1,14 +1,17 @@
 """Model representing the randomised Pac-Man behaviour"""
+from random import choice
+
 from solving_pacman_backend.models.agents.pacman_agent import PacmanAgent
 from solving_pacman_backend.models.graph import Graph
-from solving_pacman_backend.utils.agent_utils import gen_random_path
 
 
 class RandomPacMan(PacmanAgent):
     """
     Model representing the random Pac-Man behaviour.
 
-    In this model, Pac-Man will randomly choose a path.
+    In this model, Pac-Man will randomly choose a path. I have chosen to
+    implement this behaviour similar to that of a frightened ghost.
+    Pac-Man will randomly choose a new direction at each junction.
     """
 
     def __init__(
@@ -17,15 +20,13 @@ class RandomPacMan(PacmanAgent):
         super().__init__(home_path, respawn_point)
 
     def _perceive(self, time: int, level: Graph) -> None:
-        # Remove target if agent has reached position
-        if len(self.target) > 0 and self.position == self.target[0]:
-            self.target.pop(0)
-        # If there is no target, assign new target
-        if len(self.target) == 0:
-            self.target, self.path = gen_random_path(
-                level, self.position, self.move_history
-            )
-        self.path.get_next_pos()
+        all_paths = level.find_path_to_next_jct(self.position)
+        # prune paths where the path only contains the target.
+        valid_paths = [path for path in all_paths if len(path) > 2]
+        self.path = choice(valid_paths)
+        if self.path.route[0].position == self.position:
+            # if the path contains the current pos it must be removed from the list
+            self.path.get_next_pos()
 
     def _execute(self) -> tuple[int, int]:
         move = self.path.get_next_pos().position
