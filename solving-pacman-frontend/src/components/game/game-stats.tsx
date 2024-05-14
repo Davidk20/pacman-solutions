2fdd076fe5faa5f5fa9156b80f744650a79b90a3
David <davidkidd.dev@gmail.com>
2024-05-14 14:16:19 +0100

    replaced styling with tailwind and removed stylesheets

--- solving-pacman-frontend/src/components/game/game-stats.tsx
+++ solving-pacman-frontend/src/components/game/game-stats.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "../../styles/components/game/game-stats.scss";
 
 /**
  * Props for the GameStats Component.
@@ -23,16 +22,16 @@ export interface GameStatsProps {
   running: boolean,
   /**
    * Callback function to parent component allowing the game to be paused.
-  */
- toggleGame: () => void,
- /**
-  * `true` if the game simulation has finished.
-  */
- gameOver: boolean,
- /**
-  * Function to restart the game simulation.
-  */
- toggleRestart: () => void
+   */
+  toggleGame: () => void,
+  /**
+   * `true` if the game simulation has finished.
+   */
+  gameOver: boolean,
+  /**
+   * Function to restart the game simulation.
+   */
+  toggleRestart: () => void,
 }
 
 /**
@@ -42,35 +41,41 @@ export class GameStats extends React.Component<GameStatsProps>{
   override render(): React.ReactNode {
     return(
       <div
-        className="Game-Stats-Container"
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          padding: "20px",
-          textAlign: "center",
-          width: "20%",
-          border: "3px solid white",
-          borderRadius:"5px",
-          boxShadow: "5px 5px 15px 5px #000000"
-        }}
+        className="
+          flex flex-col items-center justify-center p-5 gap-2 
+          border-2 border-pacman-wall-colour rounded-md
+          text-white font-pixel text-center
+        "
       >
         {!this.props.gameOver ?
-          <div className="Button" onClick={this.props.toggleGame}>
+          <div 
+            className="
+              flex items-center justify-center bg-black cursor-pointer 
+              rounded-lg border-2 border-pacman-wall-colour
+              px-6 py-2
+            " 
+            onClick={this.props.toggleGame}
+          >
             <span>{!this.props.running ? "START" : "PAUSE"}</span>
           </div>
           :
           <div>
-            <span className="Stat-Text">GAME OVER</span>
-            <div className="Button" onClick={this.props.toggleRestart}>
+            <span>GAME OVER</span>
+            <div 
+              className="
+                flex items-center justify-center bg-black cursor-pointer 
+                rounded-lg border-2 border-pacman-wall-colour
+                px-6 py-2
+              " 
+              onClick={this.props.toggleRestart}
+            >
               <span>RESTART</span>
             </div>
           </div>
         }
-        <span className="Stat-Text">TIME: {this.props.time}</span>
-        <span className="Stat-Text">SCORE: {this.props.score}</span>
-        <span className="Stat-Text">ENERGISED: {this.props.energised.toString().toUpperCase()}</span>
+        <span>TIME: {this.props.time}</span>
+        <span>SCORE: {this.props.score}</span>
+        <span>ENERGISED: {this.props.energised.toString().toUpperCase()}</span>
       </div>
     );
   }
